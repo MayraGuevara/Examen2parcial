eefd1d6a1c8dc7a98a6a1a2c1e44cc6878b66214
Mayra Guevara <maystef1@hotmail.com>
2019-07-08 13:19:54 -0600

    creacion del boton Crear Usuario y del boton remover usuario.

--- Examen2Parcial/MainWindow.xaml.cs
+++ Examen2Parcial/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace Examen2Parcial
 {
@@ -20,9 +24,23 @@ namespace Examen2Parcial
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection sqlconnection;
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["Examen2Parcial.Properties.Settings.ERPConnectionString"].ConnectionString;
+            sqlconnection = new SqlConnection(connectionString);
+        }
+
+        private void BtnCrearUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnRemoverUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
